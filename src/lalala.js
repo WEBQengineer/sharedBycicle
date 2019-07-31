162fb9870be368c6f14636ebb48450fc3cd13898
WEBQengineer <2639114073@qq.com>
2019-07-31 10:24:26 +0800

    增加测试用lalala.js文件

---
+++ src/lalala.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App1 extends Component {
+  render(){
+    return(
+      <div>
+        123456
+      </div>
+    );
+  }
+}
+
+export default App1;
